d921edb2e31e58d74b8203883605454440569882
wxn0brP <toja.filiper@gmail.com>
2025-05-10 14:19:35 +0200

    fix: 0x27.2

--- src/shared/getRecomended.ts
+++ src/shared/getRecomended.ts
@@ -1,36 +1,10 @@
-import { existsSync } from "fs";
-import puppeteer from "puppeteer";
+import { BrowserWindow } from "electron";
 
 const regexVi = /\/vi\/([^/]+)/;
 const regexUrl = /https?:\/\/[^\s"']+/g;
 
-let browserType = process.env.BROWSER as any;
-if (!browserType) {
-    browserType = await findChromeExecutable();
-    if (browserType) browserType = "chrome";
-}
-if (!browserType) {
-    browserType = await findFirefoxExecutable();
-    if (browserType) browserType = "firefox";
-}
-if (!browserType) {
-    browserType = "chrome";
-}
-console.log("Using browser:", browserType);
-
 export async function getRecomended(id: string) {
-    const browser = await puppeteer.launch({
-        headless: true,
-        browser: browserType,
-    });
-    const page = await browser.newPage();
-
-    await page.goto(`https://www.youtube.com/watch?v=${id}`, { waitUntil: "networkidle2" });
-
-    await new Promise(resolve => setTimeout(resolve, 100));
-
-    const html = await page.content();
-    await browser.close();
+    const html: string = await dumpPageHTML(`https://www.youtube.com/watch?v=${id}`);
 
     const linksRaw = [...new Set(html.match(regexUrl))];
     const videos = linksRaw.filter(l => l.includes("i.ytimg.com/vi"));
@@ -45,49 +19,19 @@ export async function getRecomended(id: string) {
     return idsUnique;
 }
 
-async function findChromeExecutable() {
-    const paths = {
-        win32: [
-            process.env.LOCALAPPDATA + '\\Google\\Update\\ClientStateMedium\\{8A69D345-D564-463C-AFF1-A69D9E530F96}\\main.exe',
-            process.env.PROGRAMFILES + '\\Google\\Chrome\\Application\\chrome.exe',
-            process.env['PROGRAMFILES(X86)'] + '\\Google\\Chrome\\Application\\chrome.exe'
-        ],
-        darwin: [
-            '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-            '/Applications/Chromium.app/Contents/MacOS/Chromium'
-        ],
-        linux: [
-            '/usr/bin/google-chrome',
-            '/usr/bin/chromium',
-            '/usr/bin/chromium-browser'
-        ]
-    }[process.platform];
+async function dumpPageHTML(url: string) {
+    const win = new BrowserWindow({
+        show: false,
+        webPreferences: {
+            offscreen: true,
+            contextIsolation: true,
+        }
+    });
 
-    for (const path of paths) {
-        if (existsSync(path)) return path;
-    }
+    await win.loadURL(url, { userAgent: 'Mozilla/5.0' });
 
-    return null;
-}
+    await new Promise(res => setTimeout(res, 1000));
 
-async function findFirefoxExecutable() {
-    const paths = {
-        win32: [
-            process.env.PROGRAMFILES + '\\Mozilla Firefox\\firefox.exe',
-            process.env['PROGRAMFILES(X86)'] + '\\Mozilla Firefox\\firefox.exe'
-        ],
-        darwin: [
-            '/Applications/Firefox.app/Contents/MacOS/firefox'
-        ],
-        linux: [
-            '/usr/bin/firefox',
-            '/usr/local/bin/firefox'
-        ]
-    }[process.platform];
-
-    for (const path of paths) {
-        if (existsSync(path)) return path;
-    }
-
-    return null;
+    const html = await win.webContents.executeJavaScript("document.documentElement.outerHTML");
+    return html;
 }
