a9dcb34e052475fe8958c30dbf3a7e99fdfa377a
wxn0brP <toja.filiper@gmail.com>
2025-08-22 13:14:16 +0200

    feat: 0x98 - update deps

--- src/shared/gate/vql.ts
+++ src/shared/gate/vql.ts
@@ -1,103 +1,44 @@
 import { seeLogs } from "#echo/logger";
 import { getCaps, getChannelVideos, getPlaylistIds, getPlaylistInfo, searchVideo } from "#relay/apiBack";
 import { getFeed, getQuickFeed } from "#relay/feed";
+import { clearQuickCache } from "#relay/fetchQuick";
 import { getRecommended } from "#relay/getRecommended";
-import { createValtheraAdapter } from "@wxn0brp/vql";
+import { getSuggestions } from "#relay/suggestions";
+import { AdapterBuilder } from "@wxn0brp/vql/apiAbstract";
 import { runFeedVQL } from "./alg";
 import "./cache";
 import { channelInfo } from "./logic/channel";
 import { downloadVideo } from "./logic/download";
 import { fetchQuickCache, fetchQuickCache$ } from "./logic/quick";
 import { apiExecutor, retrieveVideoData, retrieveVideoData$ } from "./logic/vidInfo";
-import { getSuggestions } from "#relay/suggestions";
-import { clearQuickCache } from "#relay/fetchQuick";
 
-export const YouTubeAdapter = createValtheraAdapter({
-    async getCollections() {
-        return [
-            "playlist",
-            "channel",
-            "download",
-            "search",
-            
-            "video",
-            "video-static",
-            "video-static-quick",
-            
-            "channelVideos",
-            "recommendations",
-            "recommendationsData",
-            "channelInfo",
-            
-            "channelFeed",
-            "quickFeed",
-            "algRun",
-            
-            "self-version",
-            "video-load",
-            "seeLogs",
-        ];
-    },
+const adapter = new AdapterBuilder();
 
-    async add(collection, data) {
-        try {
-            if (collection === "download") return await downloadVideo(data);
-        } catch (e) {
-            console.error(e);
-        }
-        return {};
-    },
+adapter.add("download", async (data) => downloadVideo(data));
 
-    async find(collection, search) {
-        try {
-            if (collection === "playlistIds") return await getPlaylistIds(search.url || search._id);
-            if (collection === "recommendations") return await getRecommended(search.url || search._id, search.limit || 10);
-            if (collection === "video-static") return await retrieveVideoData$(search);
-            if (collection === "channelVideos") return await getChannelVideos(search.url || search._id, search.flat ?? true);
-            if (collection === "channelFeed") return await getFeed(search.url || search._id);
-            if (collection === "quickFeed") return await getQuickFeed();
-            if (collection === "video-static-quick") return await fetchQuickCache$(search);
-            if (collection === "suggestions") return await getSuggestions(search.q || search.query, search.hl || "en", search.gl || "US");
-            if (collection === "playlist") return await getPlaylistInfo(search.url || search._id);
-        } catch (e) {
-            console.error(e);
-        }
-        return [];
-    },
+adapter.find("playlistIds", async (search) => getPlaylistIds(search.url || search._id));
+adapter.find("recommendations", async (search) => getRecommended(search.url || search._id, search.limit || 10));
+adapter.find("video-static", async (search) => retrieveVideoData$(search));
+adapter.find("channelVideos", async (search) => getChannelVideos(search.url || search._id, search.flat ?? true));
+adapter.find("channelFeed", async (search) => getFeed(search.url || search._id));
+adapter.find("quickFeed", async () => getQuickFeed());
+adapter.find("video-static-quick", async (search) => fetchQuickCache$(search));
+adapter.find("suggestions", async (search) => getSuggestions(search.q || search.query, search.hl || "en", search.gl || "US"));
+adapter.find("playlist", async (search) => getPlaylistInfo(search.url || search._id));
 
-    async findOne(collection, search) {
-        try {
-            if (collection === "video") return await retrieveVideoData(search.url || search._id);
-            if (collection === "video-static") return await retrieveVideoData(search.url || search._id, false);
-            if (collection === "search") return await searchVideo(search.q || search.query, search.size || 10);
-            if (collection === "self-version") return { version: process.env.VOIDTUBE_VERSION || "unknown" };
-            if (collection === "channelInfo") return await channelInfo(search.url || search._id || search.id, search.update || false);
-            if (collection === "algRun") return await runFeedVQL();
-            if (collection === "seeLogs") return seeLogs();
-            if (collection === "video-static-quick") return await fetchQuickCache(search._id || search.id);
-            if (collection === "caps") return await getCaps(search.url || search._id);
-        } catch (e) {
-            console.error(e);
-        }
-        return null;
-    },
+adapter.findOne("video", async (search) => retrieveVideoData(search.url || search._id));
+adapter.findOne("video-static", async (search) => retrieveVideoData(search.url || search._id, false));
+adapter.findOne("search", async (search) => searchVideo(search.q || search.query, search.size || 10));
+adapter.findOne("self-version", async () => ({ version: process.env.VOIDTUBE_VERSION || "unknown" }));
+adapter.findOne("channelInfo", async (search) => channelInfo(search.url || search._id || search.id, search.update || false));
+adapter.findOne("algRun", async () => runFeedVQL());
+adapter.findOne("seeLogs", async () => seeLogs());
+adapter.findOne("video-static-quick", async (search) => fetchQuickCache(search._id || search.id));
+adapter.findOne("caps", async (search) => getCaps(search.url || search._id));
 
-    async removeOne(collection, search) {
-        try {
-            if (collection === "video-load") return apiExecutor.cancel(search.id || search._id || search.url);
-        } catch (e) {
-            console.error(e);
-        }
-        return false;
-    },
+adapter.removeOne("video-load", async (search) => apiExecutor.cancel(search.id || search._id || search.url));
 
-    async remove(collection, search) {
-        try {
-            if (collection === "video-load") return apiExecutor.cancelLevel(0);
-            if (collection === "clearQuickCache") return await clearQuickCache();
-        } catch (e) {
-            console.error(e);
-        }
-        return false;
-    },
-}, true);
+adapter.remove("video-load", async () => apiExecutor.cancelLevel(0));
+adapter.remove("clearQuickCache", async () => await clearQuickCache());
+
+export const YouTubeAdapter = adapter.getAdapter(true);
